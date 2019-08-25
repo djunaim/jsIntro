5bbc1ecf67e3f0375258b1ef49d6b137ae282613
Monica Djunaidi <modjun12@gmail.com>
2019-08-25 16:48:36 -0500

    added html js

--- arrays.js
+++ arrays.js
@@ -1,1 +1,34 @@
 console.log("arrays");
+
+const bubbleTea = [
+    {
+        type: 'thai',
+        description: 'orange',
+    },
+    {
+        type: 'vietnameseCoffee',
+        description: 'brown',
+    },
+    {
+        type: 'honeydew',
+        description: 'green',
+    }
+];
+
+const printToDOM = (id, textPrint) => {
+    const selectDiv = document.getElementById(id); 
+    selectDiv.innerHTML = textPrint;
+}
+
+const buildBoba = () => {
+    let bobaType = '';
+    for (let i=0; i < bubbleTea.length; i++) {
+        bobaType += `<div class="tea">`
+        bobaType += `<h3>${bubbleTea[i].type}</h3>`;
+        bobaType += `<p>${bubbleTea[i].description}</p>`
+        bobaType += `</div>`
+    }
+    printToDOM('boba', bobaType);
+}
+
+buildBoba();
